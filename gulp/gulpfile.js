e5556d772c7dd8977fc9efcdfe44233716c7da85
Gustavo de Assis <gustavoassis1991@hotmail.com>
2022-07-30 17:46:01 -0300

    finalizando modulo gulp

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,12 +1,17 @@
+const { series, parallel} = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
 const image = require('gulp-imagemin')
+const htmlmin = require('gulp-htmlmin')
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
 function tarefasCSS(cb) {
-    return gulp.src([
+    gulp.src([
             './node_modules/bootstrap/dist/css/bootstrap.css', 
             './vendor/owl/css/owl.css',
             './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
@@ -16,22 +21,26 @@ function tarefasCSS(cb) {
         .pipe(cssmin())
         .pipe(rename({ suffix: '.min'})) // libs.min.css
         .pipe(gulp.dest('./dist/css'))
+    cb()
 }
 
-function tarefasJS() {
-    return gulp.src([
+function tarefasJS(callback) {
+    gulp.src([
         './node_modules/jquery/dist/jquery.js',
         './node_modules/bootstrap/dist/js/bootstrap.js',
         './vendor/owl/js/owl.js',
         './vendor/jQuery-Mask-Plugin-master/dist/jquery.mask.js',
-        './vendor/jquery-ui/jquery-ui.js',
+        //'./vendor/jquery-ui/jquery-ui.js',
         './src/js/customizado.js'
-
-    ])
-    .pipe(concat('scripts.js'))
-    .pipe(uglify())
-    .pipe(rename({ suffix: '.min'})) // libs.min.js
-    .pipe(gulp.dest('./dist/js'))
+        ])
+        .pipe(babel({
+            comments: false,
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min'})) // libs.min.js
+        .pipe(gulp.dest('./dist/js'))
+    return(callback)
 }
 
 function tarefasImagem() {
@@ -50,6 +59,32 @@ function tarefasImagem() {
         .pipe(gulp.dest('./dist/images'))
 }
 
+// POC - Proof of Concept
+function tarefasHTML(callback) {
+    gulp.src('./src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true}))
+        .pipe(gulp.dest('./dist'))
+
+    return callback()
+}
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    })
+    gulp.watch('src/**/*').on('change', process)
+    gulp.watch('src/**/*').on('change', reload)
+})
+
+function end(cb){
+    console.log("Tarefa finalizada")
+    return cb()
+}
+
+const process = parallel( tarefasHTML, tarefasCSS, tarefasJS, end )
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
 exports.images = tarefasImagem
+exports.default = process
